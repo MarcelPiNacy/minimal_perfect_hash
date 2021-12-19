009121a3fbe2892eb61ef76955baf728ca619407
MarcelPiNacy <marcelpi97@gmail.com>
2021-12-19 22:22:29 +0100

    Update mph.hpp

--- mph.hpp
+++ mph.hpp
@@ -1,12 +1,25 @@
-#pragma once
+/*
+	Copyright 2021 Marcel Pi Nacy
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+		http://www.apache.org/licenses/LICENSE-2.0
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+#ifndef MPH_INCLUDED
+#define MPH_INCLUDED
+
 #include <cstddef>
 #include <cstdint>
 #include <cassert>
 #include <algorithm>
 #include <type_traits>
 #include <string_view>
-
-
 
 namespace mph
 {
@@ -579,3 +592,4 @@ namespace mph
 		return hash_map<sizeof...(U) + 1, K, V, H, E, S>(std::move(first), std::forward<U>(values)...);
 	}
 }
+#endif
